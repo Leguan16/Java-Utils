b7fdb7719065353558526fe5c5d58103b0705791
Sammster10 <76685474+Sammster10@users.noreply.github.com>
2022-06-22 21:07:19 -0400

    Update TickUtils.java

--- minecraftUtils/TickUtils.java
+++ minecraftUtils/TickUtils.java
@@ -52,6 +52,8 @@ public class TickUtils {
             ticksRecorded++;
             if (ticksRecorded >= ticks) break;
         }
+        System.out.println(ticksRecorded);
+        if (ticksRecorded == 0) return 0;
         return (double) total / ticksRecorded;
     }
 
